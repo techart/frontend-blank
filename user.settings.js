1c01d45924328dce3fa236fe1904524261b5fa6b
Сафронов Виктор <safronov@techart.ru>
2016-05-25 11:21:38 +0300

    Изменения в конфигурации дял возможности подключения bitrix

--- user.settings.js
+++ user.settings.js
@@ -1,42 +1,47 @@
 var child_process = require('child_process');
-
-var getusername = function() {
-    try {
-        return String(child_process.execSync("whoami", {encoding: 'utf8'})).trim();
-    } catch (e) {
-        return null;
-    }
-}
+var path = require('path');
 
 module.exports = {
-    site: "milanodoors2.com",
-    port: 8888,
-    docroot: '../www',
-    hotPort: 8889,
-    entry: {
-        index: ['./src/index.js'],
-        img: ['./src/img.js']
-        // main: ['./src/page/main/main.js']
+	site: "sitename",
+	port: 8888,
+	docRoot: '../../../..', // '../www'
+	buildPath: '../builds', // '../www/builds'
+	hotPort: 8889,
+	entry: {
+		index: ['./src/index.js'],
+		img: ['./src/img.js']
+		// main: ['./src/page/main/main.js']
 
-    },
-    name: getusername(),
-
-    devHost: function() {
-        return this.site + "." + this.name + ".techart.intranet";
-    },
-    devUrl: function() {
-        return "http://" + this.devHost() + ":" + this.port;
-    },
-    titanHost: function() {
-        return this.site + ".projects.techart.ru";
-    },
-    titanUrl: function() {
-        return "http://" + this.titanHost();
-    },
-    hotHost: function() {
-        return this.devHost();
-    },
-    hotUrl: function() {
-        return 'http://' + this.hotHost() + (this.hotPort ? ':' + this.hotPort : '');
-    }
-}
+	},
+	getPublicPath: function getPublicPath(env = 'prod') {
+		return this.getBuildPath(env).replace(path.resolve(this.docRoot), '');
+	},
+	getBuildPath: function getBuildPath(env = 'prod') {
+		return path.resolve(this.buildPath, env != 'hot' ? env : 'dev');
+	},
+	getUserName: function getUserName() {
+		try {
+			return String(child_process.execSync("whoami", {encoding: 'utf8'})).trim();
+		} catch (e) {
+			return null;
+		}
+	},
+	devHost: function () {
+		return this.site + "." + this.getUserName() + ".techart.intranet";
+	},
+	devUrl: function () {
+		return "http://" + this.devHost() + ":" + this.port;
+	},
+	titanHost: function () {
+		return this.site + ".projects.techart.ru";
+	},
+	titanUrl: function () {
+		return "http://" + this.titanHost();
+	},
+	hotHost: function () {
+		return this.devHost();
+	},
+	hotUrl: function () {
+		return 'http://' + this.hotHost() + (this.hotPort ? ':' + this.hotPort : '');
+	}
+};
