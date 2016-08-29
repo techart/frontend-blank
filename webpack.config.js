343e5fe0e5c7edfffbd782cb89f4a0d9f1a384a2
Svistunov SS <svistunov@techart.ru>
2016-08-29 14:46:51 +0300

    settings exposeGlobal & aliasGlobal

--- webpack.config.js
+++ webpack.config.js
@@ -17,28 +17,28 @@ var AssetsPlugin = require('assets-webpack-plugin');
 var styleLintPlugin = require('stylelint-webpack-plugin');
 
 var addHash = function addTemplateHash(template, hash, devHash) {
-	devHash = devHash || hash;
-	return  (production && userSettings.hash.prod ? template.replace(/\.[^.]+$/, `/[${hash}:3]/[${hash}]$&`) :
-		(userSettings.hash.dev ? `${template}?h=[${devHash}]` : template));
+    devHash = devHash || hash;
+    return (production && userSettings.hash.prod ? template.replace(/\.[^.]+$/, `/[${hash}:3]/[${hash}]$&`) :
+        (userSettings.hash.dev ? `${template}?h=[${devHash}]` : template));
 };
 
 var doiusePartialSupport = function doiusePartialSupportIgnore(usageInfo) {
-	if (usageInfo.featureData.missingData) {
-		var data = usageInfo.featureData.missingData;
-		var onlyPartialSupport = true;
-		for (brName in data) {
-			for (vrName in data[brName]) {
-				if (data[brName][vrName] != 'a') {
-					onlyPartialSupport = false;
-					break;
-				}
-			}
-		}
-		if (onlyPartialSupport) {
-			return false;
-		}
-	}
-	return true;
+    if (usageInfo.featureData.missingData) {
+        var data = usageInfo.featureData.missingData;
+        var onlyPartialSupport = true;
+        for (brName in data) {
+            for (vrName in data[brName]) {
+                if (data[brName][vrName] != 'a') {
+                    onlyPartialSupport = false;
+                    break;
+                }
+            }
+        }
+        if (onlyPartialSupport) {
+            return false;
+        }
+    }
+    return true;
 };
 
 
@@ -53,244 +53,265 @@ var imgCommonFolder = 'img/common';
 
 var plugins = [
 
-	new styleLintPlugin({
-		syntax: mainStyleType,
-		files: ['**/*.s?(a|c)ss', '**/*.less']
-	}),
+    new styleLintPlugin({
+        syntax: mainStyleType,
+        files: ['**/*.s?(a|c)ss', '**/*.less']
+    }),
 
-	new SvgStore(
-		[
-			path.join('.', 'img', 'sprite', 'svg', '**/*.svg')
-		],
-		'img',
-		{
-			name: 'sprite/sprite.svg',
-			prefix: 's-',
-			svgoOptions: {
-				plugins: [
-					{removeTitle: true}
-				]
-			}
-		}
-	),
-	new SpritesmithPlugin({
-		src: {
-			cwd: path.resolve(__dirname, 'img/sprite/png'),
-			glob: '**/*.*'
-		},
-		target: {
-			image: path.resolve(__dirname, 'img/sprite/sprite.png'),
-			css: path.resolve(__dirname, 'src/style/' + (mainStyleType == 'scss' ? '_' : '') + 'sprite.' + mainStyleType)
-		},
-		apiOptions: {
-			cssImageRef: "~img/sprite/sprite.png"
-		}
-	}),
+    new SvgStore(
+        [
+            path.join('.', 'img', 'sprite', 'svg', '**/*.svg')
+        ],
+        'img',
+        {
+            name: 'sprite/sprite.svg',
+            prefix: 's-',
+            svgoOptions: {
+                plugins: [
+                    {removeTitle: true}
+                ]
+            }
+        }
+    ),
+    new SpritesmithPlugin({
+        src: {
+            cwd: path.resolve(__dirname, 'img/sprite/png'),
+            glob: '**/*.*'
+        },
+        target: {
+            image: path.resolve(__dirname, 'img/sprite/sprite.png'),
+            css: path.resolve(__dirname, 'src/style/' + (mainStyleType == 'scss' ? '_' : '') + 'sprite.' + mainStyleType)
+        },
+        apiOptions: {
+            cssImageRef: "~img/sprite/sprite.png"
+        }
+    }),
 
-	new BowerWebpackPlugin({
-		searchResolveModulesDirectories: false
-	}),
+    new BowerWebpackPlugin({
+        searchResolveModulesDirectories: false
+    }),
 
-	new webpack.DefinePlugin({
-		NODE_ENV: JSON.stringify(env)
-	}),
+    new webpack.DefinePlugin({
+        NODE_ENV: JSON.stringify(env)
+    }),
 
-	new webpack.optimize.CommonsChunkPlugin({
-		name: Object.keys(userSettings.entry)[0], // Move dependencies to our main file
-		chunks: Object.keys(userSettings.entry),
-		// children:  true, // Look for common dependencies in all children, // BUG!
-		minChunks: 2 // How many times a dependency must come up before being extracted
-	}),
+    new webpack.optimize.CommonsChunkPlugin({
+        name: Object.keys(userSettings.entry)[0], // Move dependencies to our main file
+        chunks: Object.keys(userSettings.entry),
+        // children:  true, // Look for common dependencies in all children, // BUG!
+        minChunks: 2 // How many times a dependency must come up before being extracted
+    }),
 
-	new ExtractTextPlugin(
-		addHash('css/[name].css', 'contenthash'), {
-			allChunks: true // false
-		}
-	),
+    new ExtractTextPlugin(
+        addHash('css/[name].css', 'contenthash'), {
+            allChunks: true // false
+        }
+    ),
 
-	new webpack.ProvidePlugin({
-		$: "jquery",
-		jQuery: "jquery",
-		"window.jQuery": "jquery"
-	}),
+    new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery",
+        "window.jQuery": "jquery"
+    }),
 
-	new webpack.ResolverPlugin([
-		new ComponentResolverPlugin(
-			['js', 'jsx', 'less', 'sass', 'scss']
-			// array of extensions e.g `['js']` (default: `['jsx', 'js']`)
-		)
-	]),
+    new webpack.ResolverPlugin([
+        new ComponentResolverPlugin(
+            ['js', 'jsx', 'less', 'sass', 'scss']
+            // array of extensions e.g `['js']` (default: `['jsx', 'js']`)
+        )
+    ]),
 
-	new AssetsPlugin({
-		filename: path.join('assets', env + '.json'),
-		path: __dirname,
-		prettyPrint: true
-	})
+    new AssetsPlugin({
+        filename: path.join('assets', env + '.json'),
+        path: __dirname,
+        prettyPrint: true
+    })
 
 ];
 
 if (production) {
-	plugins = plugins.concat([
+    plugins = plugins.concat([
 
-		// This plugin looks for similar chunks and files
-		// and merges them for better caching by the user
-		new webpack.optimize.DedupePlugin(),
+        // This plugin looks for similar chunks and files
+        // and merges them for better caching by the user
+        new webpack.optimize.DedupePlugin(),
 
-		// This plugins optimizes chunks and modules by
-		// how much they are used in your app
-		new webpack.optimize.OccurenceOrderPlugin(),
+        // This plugins optimizes chunks and modules by
+        // how much they are used in your app
+        new webpack.optimize.OccurenceOrderPlugin(),
 
-		// This plugin prevents Webpack from creating chunks
-		// that would be too small to be worth loading separately
-		// new webpack.optimize.MinChunkSizePlugin({
-		//     minChunkSize: 1000, // ~5kb
-		// }),
+        // This plugin prevents Webpack from creating chunks
+        // that would be too small to be worth loading separately
+        // new webpack.optimize.MinChunkSizePlugin({
+        //     minChunkSize: 1000, // ~5kb
+        // }),
 
-		// new webpack.optimize.AggressiveMergingPlugin(),
+        // new webpack.optimize.AggressiveMergingPlugin(),
 
-		// This plugin minifies all the Javascript code of the final bundle
-		new webpack.optimize.UglifyJsPlugin({
-			mangle: true,
-			compress: {
-				warnings: false // Suppress uglification warnings
-			}
-		}),
+        // This plugin minifies all the Javascript code of the final bundle
+        new webpack.optimize.UglifyJsPlugin({
+            mangle: true,
+            compress: {
+                warnings: false // Suppress uglification warnings
+            }
+        }),
 
-		// This plugins defines various variables that we can set to false
-		// in production to avoid code related to them from being compiled
-		// in our final bundle
-		new webpack.DefinePlugin({
-			__SERVER__: !production,
-			__DEVELOPMENT__: !production,
-			__DEVTOOLS__: !production,
-			'process.env': {
-				BABEL_ENV: JSON.stringify(process.env.NODE_ENV)
-			}
-		}),
+        // This plugins defines various variables that we can set to false
+        // in production to avoid code related to them from being compiled
+        // in our final bundle
+        new webpack.DefinePlugin({
+            __SERVER__: !production,
+            __DEVELOPMENT__: !production,
+            __DEVTOOLS__: !production,
+            'process.env': {
+                BABEL_ENV: JSON.stringify(process.env.NODE_ENV)
+            }
+        }),
 
-		new webpack.NoErrorsPlugin()
+        new webpack.NoErrorsPlugin()
 
-	]);
+    ]);
 } else {
-	plugins = plugins.concat([
-		new CleanWebpackPlugin(path.resolve(userSettings.getBuildPath(env)), {
-			root: path.resolve(__dirname, '..')  //TODO: is this OK?
-		}),
-	]);
-	// plugins = plugins.concat([
-	//     new webpack.SourceMapDevToolPlugin({
-	//         filename: '[file].map',
-	//         // columns: false,
-	//         module: true,
-	//         moduleFilenameTemplate: "[absolute-resource-path]",
-	//         fallbackModuleFilenameTemplate: "[absolute-resource-path]"
-	//     }
-	//         // '[file].map', null,
-	//         // "[absolute-resource-path]", "[absolute-resource-path]"
-	//     )
-	// ]);
+    plugins = plugins.concat([
+        new CleanWebpackPlugin(path.resolve(userSettings.getBuildPath(env)), {
+            root: path.resolve(__dirname, '..')  //TODO: is this OK?
+        }),
+    ]);
+    // plugins = plugins.concat([
+    //     new webpack.SourceMapDevToolPlugin({
+    //         filename: '[file].map',
+    //         // columns: false,
+    //         module: true,
+    //         moduleFilenameTemplate: "[absolute-resource-path]",
+    //         fallbackModuleFilenameTemplate: "[absolute-resource-path]"
+    //     }
+    //         // '[file].map', null,
+    //         // "[absolute-resource-path]", "[absolute-resource-path]"
+    //     )
+    // ]);
 }
 
-module.exports = {
-		debug: !production,
-		devtool: production ? false : 'source-map',
-		devtoolModuleFilenameTemplate: "[absolute-resource-path]",
-		devtoolFallbackModuleFilenameTemplate: "[absolute-resource-path]",
-		entry: userSettings.entry,
-		output: {
-			path: userSettings.getBuildPath(env),
-			filename: addHash('js/[name].js', 'chunkhash', 'hash'),
-			chunkFilename: addHash('js/[name].js', 'chunkhash', 'hash'),
-			publicPath: userSettings.getPublicPath(env)
-		},
+var _export = {
+    debug: !production,
+    devtool: production ? false : 'source-map',
+    devtoolModuleFilenameTemplate: "[absolute-resource-path]",
+    devtoolFallbackModuleFilenameTemplate: "[absolute-resource-path]",
+    entry: userSettings.entry,
+    output: {
+        path: userSettings.getBuildPath(env),
+        filename: addHash('js/[name].js', 'chunkhash', 'hash'),
+        chunkFilename: addHash('js/[name].js', 'chunkhash', 'hash'),
+        publicPath: userSettings.getPublicPath(env)
+    },
 
-	resolve: {
-		root: path.resolve('./src'),
-			extensions: ['', '.js', '.less', '.sass', '.scss'],
-			alias: {
-			img: 'img',
-				font: 'font'
-		},
-		fallback: ['.', 'img'],
-		modulesDirectories: ["web_modules", "node_modules", "bower_components"]
-	},
+    resolve: {
+        root: path.resolve('./src'),
+        extensions: ['', '.js', '.less', '.sass', '.scss'],
+        alias: {
+            img: 'img',
+            font: 'font'
+        },
+        fallback: ['.', 'img'],
+        modulesDirectories: ["web_modules", "node_modules", "bower_components"]
+    },
 
-	plugins: plugins,
-		module: {
-		// noParse: [],
-		preLoaders: [
-			{
-				test: /\.js$/,
-				loader: 'component-css?ext='+ userSettings.mainStyleType + '!eslint'
-			}
-		],
-		loaders: [
-			{ test: require.resolve('jquery'), loader: 'expose?jQuery!expose?$' },
-			{
-				test: /\.js$/,
-				loader: 'babel',
-				include: __dirname + '/src',
-				query: {
-					presets: ['es2015']
-				}
-			},
-			// Extract css files
-			{
-				test: /\.css$/,
-				loader: env != 'hot' ? ExtractTextPlugin.extract("style", "css") : 'style!' + styles
-			},
-			{
-				test: /\.(scss|sass)$/,
-				// loader: 'style!css?sourceMap!sass?sourceMap'
-				loader: env != 'hot' ? ExtractTextPlugin.extract('style', sassStyle) : 'style!' + sassStyle
-			},
-			{
-				test: /\.less$/,
-				// loader: 'style!css?sourceMap!less?sourceMap'
-				loader: env != 'hot' ? ExtractTextPlugin.extract('style', lessStyle) : 'style!' + lessStyle
-			},
-			{
-				test: /\.html$/,
-				loader: 'html',
-			},
-			{
-				test: /\.(png|gif|jpe?g|svg)$/i,
-				include: path.resolve(__dirname, imgCommonFolder),
-				loaders: [
-					'file?name=[path][name].[ext]',
-					imageLoader
-				]
-			},
-			{
-				test: /\.(png|gif|jpe?g|svg)$/i,
-				exclude: path.resolve(__dirname, imgCommonFolder),
-				loaders: [
-					'url?limit='+fileLimit+'&name=[path][name].[ext]',
-					imageLoader
-				]
-			},
-			{
-				test: /\.woff2?(\?\S*)?$/i,
-				loader: 'url?limit='+fileLimit+',name=[path][name].[ext]',
-			},
-			{
-				test: /\.ttf|eot(\?\S*)?$/,
-				loader: 'file?name=[path][name].[ext]'
-			}
-		]
-	},
+    plugins: plugins,
+    module: {
+        // noParse: [],
+        preLoaders: [
+            {
+                test: /\.js$/,
+                loader: 'component-css?ext=' + userSettings.mainStyleType + '!eslint'
+            }
+        ],
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel',
+                include: __dirname + '/src',
+                query: {
+                    presets: ['es2015']
+                }
+            },
+            // Extract css files
+            {
+                test: /\.css$/,
+                loader: env != 'hot' ? ExtractTextPlugin.extract("style", "css") : 'style!' + styles
+            },
+            {
+                test: /\.(scss|sass)$/,
+                // loader: 'style!css?sourceMap!sass?sourceMap'
+                loader: env != 'hot' ? ExtractTextPlugin.extract('style', sassStyle) : 'style!' + sassStyle
+            },
+            {
+                test: /\.less$/,
+                // loader: 'style!css?sourceMap!less?sourceMap'
+                loader: env != 'hot' ? ExtractTextPlugin.extract('style', lessStyle) : 'style!' + lessStyle
+            },
+            {
+                test: /\.html$/,
+                loader: 'html',
+            },
+            {
+                test: /\.(png|gif|jpe?g|svg)$/i,
+                include: path.resolve(__dirname, imgCommonFolder),
+                loaders: [
+                    'file?name=[path][name].[ext]',
+                    imageLoader
+                ]
+            },
+            {
+                test: /\.(png|gif|jpe?g|svg)$/i,
+                exclude: path.resolve(__dirname, imgCommonFolder),
+                loaders: [
+                    'url?limit=' + fileLimit + '&name=[path][name].[ext]',
+                    imageLoader
+                ]
+            },
+            {
+                test: /\.woff2?(\?\S*)?$/i,
+                loader: 'url?limit=' + fileLimit + ',name=[path][name].[ext]',
+            },
+            {
+                test: /\.ttf|eot(\?\S*)?$/,
+                loader: 'file?name=[path][name].[ext]'
+            }
+        ]
+    },
 
-	postcss: function () {
-		return [
-			autoprefixer({
-				browsers: userSettings.browsers
-			}),
-			doiuse({
-				// ignore: ['rem'],
-				browsers: userSettings.browsers,
-				onFeatureUsage: doiusePartialSupport
-			})
+    postcss: function () {
+        return [
+            autoprefixer({
+                browsers: userSettings.browsers
+            }),
+            doiuse({
+                // ignore: ['rem'],
+                browsers: userSettings.browsers,
+                onFeatureUsage: doiusePartialSupport
+            })
 
-		];
-	}
+        ];
+    }
 };
+
+if (userSettings.exposeGlobal) {
+    userSettings.exposeGlobal.forEach(function (item) {
+        var query = [];
+        item.names.forEach(function (name) {
+            query.push('expose?' + name);
+        });
+        _export.module.loaders.unshift({
+            test: require.resolve(item.module),
+            loader: query.join('!')
+        })
+    });
+}
+
+if (userSettings.aliasGlobal) {
+    userSettings.aliasGlobal.forEach(function (name) {
+        _export.resolve.alias[name] = require.resolve('jquery')
+    });
+    console.log(_export.resolve.alias);
+}
+
+module.exports = _export;
