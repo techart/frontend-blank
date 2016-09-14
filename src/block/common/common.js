400b40f6f15d08af3c4837ef3bfad5ee72e7feab
Anton Shvydkoy <shvydkoy@techart.ru>
2016-09-14 11:51:09 +0300

    blocks/common авто подключение с криптов только первого уровня вложенности. Исключение twig шаблонов из выбора

--- src/block/common/common.js
+++ src/block/common/common.js
@@ -1,5 +1,8 @@
 function requireAll(r) {
-    r.keys().forEach(r);
+    r.keys().map(function(key) {
+        if (key.split('/').length <= 3) {
+            return r(key);
+        }
+    });
 }
-
-requireAll(require.context('.', true));
+requireAll(require.context('.', true, /[^twig]$/));
